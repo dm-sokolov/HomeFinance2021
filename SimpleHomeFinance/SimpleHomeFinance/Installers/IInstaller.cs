8fdc8758daa3f8a90e165b345dd04b1da88b68f1
Dmitriy Sokolov <sda.falcon@gmail.com>
2021-04-10 14:19:43 +0500

    вынес сервисы в отдельные инсталлеры

---
+++ SimpleHomeFinance/SimpleHomeFinance/Installers/IInstaller.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleHomeFinance.Installers
+{
+    public interface IInstaller
+    {
+        void InstallServices(IServiceCollection services, IConfiguration configuration);
+    }
+}
--- SimpleHomeFinance/SimpleHomeFinance/Startup.cs
+++ SimpleHomeFinance/SimpleHomeFinance/Startup.cs
@@ -15,6 +15,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Routing.Template;
 using Microsoft.OpenApi.Models;
+using SimpleHomeFinance.Installers;
 using SimpleHomeFinance.Options;
 
 namespace SimpleHomeFinance
@@ -31,23 +32,7 @@ namespace SimpleHomeFinance
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<DataContext>();
-            services.AddControllersWithViews();
-            services.AddRazorPages();
-
-            services.AddSwaggerGen(x =>
-            {
-                x.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Simple Home Finance API",
-                    Version = "v1"
-                });
-                
-            });
+            services.InstallServicesInAssembly(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
